67208a28a84bb96b3e0ef17949bef0f3aa7ece15
cuswan022 <cuswan022@student.wethinkcode.co.za>
2024-03-04 19:02:53 +0200

    Added SQL Database for storage of Notes and Maps

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql")
 
 const app = express();
+
+
+var con =  mysql.createConnection({
+    host: 'localhost',
+    port: 5051,
+    user: 'root',
+    password: '',
+    database:'cuantest'
+}
+)
+con.connect(function(err){
+  if(err) throw err;
+  console.log("Connected");
+  var sql = "INSERT INTO people (name, age) values ('Rod', 63)"
+  con.query(sql, function(err, result){
+    if (err) throw err;
+    console.log("1 record inserted")
+  })
+})
+
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
