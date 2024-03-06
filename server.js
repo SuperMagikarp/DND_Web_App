da51cf70aa8daebda81f132c495fb50f73e4b546
cuswan022 <cuswan022@student.wethinkcode.co.za>
2024-03-06 21:45:48 +0200

    Added some digital art to the header

--- server.js
+++ server.js
@@ -8,20 +8,11 @@ const app = express();
 var con =  mysql.createConnection({
     host: 'localhost',
     port: 5051,
-    user: 'root',
+    user: 'SuperMagikarp',
     password: '',
     database:'cuantest'
 }
 )
-con.connect(function(err){
-  if(err) throw err;
-  console.log("Connected");
-  var sql = "INSERT INTO people (name, age) values ('Rod', 63)"
-  con.query(sql, function(err, result){
-    if (err) throw err;
-    console.log("1 record inserted")
-  })
-})
 
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
@@ -29,8 +20,18 @@ app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
+
 app.post('/*', (req, res) => {
-    res.send('Got a POST request')
+  con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected");
+    var sql = "INSERT INTO people (name, age) values ('Rod', 63)"
+    con.query(sql, function(err, result){
+      if (err) throw err;
+      res.send('Got a POST request')
+    })
+  })
+  
   })
 
 app.listen(process.env.PORT || 5050, () => console.log("Server Running"));
